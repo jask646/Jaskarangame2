0d8999b826d04bb2d44c340d31f668ae52145c71
jaskaran2016 <80736653+jaskaran2016@users.noreply.github.com>
2021-05-24 21:49:51 +0800

    Item commitment

--- src/main/java/ass3/mygame2/ItemCreation.java
+++ src/main/java/ass3/mygame2/ItemCreation.java
@@ -29,11 +29,14 @@ public class ItemCreation
         key = new Item("key", "It has a shape of a heart", 100);
         frontGateKey = new Item("frontGateKey", "To open the front gate door", 100);
         item1 = new Item("item1", "This item is for testing", 100);
+        item2 = new Item("item2", "THis is also new test for git", 100);
         
         allItemsInGame.add(excaliburSword);
         allItemsInGame.add(key);
         allItemsInGame.add(frontGateKey);
         allItemsInGame.add(item1);
+        allItemsInGame.add(item2);
+        
     }
    
     public Item getItem(String stringItem){
